3a80640607cb4c18d6b7fc54c546371c7ac4f657
jchen8tw <11806483+jchen8tw@users.noreply.github.com>
2023-12-18 22:12:16 +0800

    add alpha minimax hybrid strategy

--- classical_policies/minimax.py
+++ classical_policies/minimax.py
@@ -1,10 +1,14 @@
 from constants import Player
+from typing import Dict
 import numpy as np
 from .base import PolicyBase
+from .alpha_zero.ewn.pytorch.NNet import NNetWrapper
+from .alpha_zero.ewn.EWNGame import EWNGame
 
 
 class ExpectiMinimaxAgent(PolicyBase):
-    def __init__(self, max_depth: int, cube_layer: int, board_size: int, heuristic='hybrid', **kwargs):
+    def __init__(self, max_depth: int, cube_layer: int,
+                 board_size: int, heuristic='hybrid', **kwargs):
         from envs import MinimaxEnv
         self.max_depth = max_depth
         self.env = MinimaxEnv(
@@ -17,6 +21,136 @@ class ExpectiMinimaxAgent(PolicyBase):
 
         if self.env.check_win() or depth == 0:
             return self.env.evaluate(heuristic=self.heuristic), None
+
+        # Maximizing player
+        if player == self.env.agent_player:
+            best_val: float = -float('inf')
+            best_action = None
+            legal_actions = self.env.get_legal_actions(player)
+            curr_dice_roll = self.env.dice_roll
+            for action in legal_actions:
+                self.env.set_dice_roll(curr_dice_roll)
+                self.env.make_simulated_action(player, action)
+                val, _ = self.expectiminimax(
+                    depth - 1, Player.CHANCE, player, alpha, beta)
+                self.env.undo_simulated_action()
+                if val > best_val:
+                    best_val = val
+                    best_action = action
+                alpha = max(alpha, best_val)
+                if beta <= alpha:
+                    break
+            return best_val, best_action
+        # Minimizing player
+        elif player == Player.get_opponent(self.env.agent_player):
+            worst_val = float('inf')
+            worst_action = None
+            legal_actions = self.env.get_legal_actions(player)
+            curr_dice_roll = self.env.dice_roll
+            for action in legal_actions:
+                self.env.set_dice_roll(curr_dice_roll)
+                self.env.make_simulated_action(player, action)
+                val, _ = self.expectiminimax(
+                    depth - 1, Player.CHANCE, player, alpha, beta)
+                self.env.undo_simulated_action()
+                if val < worst_val:
+                    worst_val = val
+                    worst_move = action
+                beta = min(beta, worst_val)
+                if beta <= alpha:
+                    break
+            return worst_val, worst_action
+        # Chance node
+        elif player == Player.CHANCE:
+            expected_val = 0
+            next_player = self.env.agent_player if parent == Player.get_opponent(
+                self.env.agent_player) else Player.get_opponent(self.env.agent_player)
+            for dice_roll in range(1, 7):
+                self.env.set_dice_roll(dice_roll)
+                val, _ = self.expectiminimax(
+                    depth - 1, next_player, player, alpha, beta)
+                expected_val += val / 6
+            return expected_val, None
+
+    def restore_env_with_obs(self, obs):
+        self.env.dice_roll = obs['dice_roll']
+        self.env.board[:] = obs['board']
+
+        for i in range(self.env.cube_num * 2):
+            self.env.cube_pos[i] = np.ma.masked
+        for i in range(self.env.board.shape[0]):
+            for j in range(self.env.board.shape[1]):
+                cube = self.env.board[i, j]
+                if cube > 0:
+                    self.env.cube_pos[cube - 1] = (i, j)
+                elif cube < 0:
+                    self.env.cube_pos[cube] = (i, j)
+
+    def predict(self, obs, **kwargs):
+        self.restore_env_with_obs(obs)
+        _, chosen_action = self.expectiminimax(
+            self.max_depth, self.env.agent_player, None, -float('inf'), float('inf'))
+        return chosen_action, None
+
+
+class AlphaZeroMinimaxAgent(PolicyBase):
+    """Using alpha zero's neural network to predict the value of current state
+    """
+
+    def __init__(self, max_depth: int, cube_layer: int,
+                 board_size: int,
+                 model_folder: str = 'alpha_zero_models',
+                 model_name: str = 'checkpoint_242.pth.tar',
+                 **policy_kwargs):
+        self.max_depth = max_depth
+        self.board_size = board_size
+        self.cube_layer = cube_layer
+        from envs import MinimaxEnv
+
+        self.env = MinimaxEnv(
+            cube_layer=cube_layer,
+            board_size=board_size)
+        game = EWNGame(board_size, cube_layer)
+        self.nnet = NNetWrapper(game)
+        self.nnet.load_checkpoint(
+            folder=model_folder,
+            filename=model_name
+        )
+
+    def ewn_obs_one_hot(
+            self, obs: Dict[str, np.ndarray | int]) -> Dict[str, np.ndarray]:
+        """ convert obs to one hot encoding
+        """
+        cube_num: int = self.cube_layer * (self.cube_layer + 1) // 2
+        board = np.zeros(
+            (self.board_size,
+             self.board_size,
+             cube_num),
+            dtype=int)
+        for i in range(self.board_size):
+            for j in range(self.board_size):
+                if obs["board"][i, j] > 0:
+                    board[i, j, obs["board"][i, j] - 1] = 1
+                elif obs["board"][i, j] < 0:
+                    board[i, j, -obs["board"][i, j] - 1] = -1
+
+        dice_roll = np.zeros(cube_num, dtype=int)
+        dice_roll[obs["dice_roll"] - 1] = 1
+        return {"board": board, "dice_roll": dice_roll}
+
+    def evaluate(self) -> float:
+        obs: Dict[str, np.ndarray] = {
+            "board": self.env.board,
+            "dice_roll": self.env.dice_roll
+        }
+        pi, v = self.nnet.predict(self.ewn_obs_one_hot(obs))
+        return v
+
+    def expectiminimax(self, depth: int, player: Player,
+                       parent: Player | None, alpha: int, beta: int):
+
+        if self.env.check_win() or depth == 0:
+            return self.evaluate(), None
 
         # Maximizing player
         if player == self.env.agent_player:
---
+++ eval_alpha_minimax.py
@@ -0,0 +1,117 @@
+from classical_policies import AlphaZeroMinimaxAgent
+import numpy as np
+from constants import ClassicalPolicy
+from statsmodels.stats.proportion import proportion_confint
+from tqdm import trange
+import argparse
+import gymnasium as gym
+from gymnasium.envs.registration import register
+
+register(
+    id='EWN-v0',
+    entry_point='envs:EinsteinWuerfeltNichtEnv'
+)
+
+
+def evaluation(env, model, render_last, eval_num=100) -> np.ndarray:
+    score = np.zeros(eval_num)
+
+    # Run eval_num times rollouts
+    for seed in trange(eval_num):
+        done = False
+        # Set seed and reset env using Gymnasium API
+        obs, info = env.reset(seed=seed)
+        reward = 0
+
+        while not done:
+            # Interact with env using Gymnasium API
+            action, _state = model.predict(obs, deterministic=True)
+            obs, reward, done, _, info = env.step(action)
+
+        # Render the last board state of each episode
+        # print("Last board state:")
+        # env.render()
+        # The episode number is same as the seed
+        episode = seed
+        score[episode] = reward
+
+    # Render last rollout
+    if render_last:
+        print("Rendering last rollout")
+        done = False
+        obs, info = env.reset(seed=eval_num - 1)
+        env.render()
+
+        while not done:
+            action, _state = model.predict(obs, deterministic=True)
+            obs, reward, done, _, info = env.step(action)
+            env.render()
+
+    return score
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description='Evaluate minimax agent', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--max_depth', type=int, default=3,
+                        help='Max search depth')
+    parser.add_argument('--num', type=int, default=100,
+                        help='Number of rollouts')
+    parser.add_argument('--render_last', action='store_true',
+                        help='Render last rollout', default=False)
+    parser.add_argument('--cube_layer', type=int, default=3,
+                        help='Number of cube layers')
+    parser.add_argument('--board_size', type=int, default=5,
+                        help='Board size')
+    parser.add_argument('--significance_level', type=float, default=0.05,
+                        help='Board size')
+    parser.add_argument('--opponent_policy', type=ClassicalPolicy.from_string, default=ClassicalPolicy.random, choices=list(ClassicalPolicy),
+                        help='Opponent policy')
+    parser.add_argument('--model_folder', type=str, default='alpha_zero_models',
+                        help='folder of model')
+    parser.add_argument('--model_name', type=str, default='checkpoint_242.pth.tar',
+                        help='name of model')
+    parser.add_argument(
+        "--num_simulations",
+        type=int,
+        default=10,
+        help="Number of simulations for mcts.")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    args = parse_args()
+
+    env = gym.make(
+        'EWN-v0',
+        cube_layer=args.cube_layer,
+        board_size=args.board_size,
+        opponent_policy=args.opponent_policy,
+        # render_mode='human',
+        max_depth=args.max_depth,
+        model_name=args.model_name,
+        model_folder=args.model_folder,
+        num_simulations=args.num_simulations,
+    )
+
+    agent = AlphaZeroMinimaxAgent(
+        # max_depth=args.max_depth,
+        max_depth=2,
+        cube_layer=args.cube_layer,
+        board_size=args.board_size,
+        model_name=args.model_name,
+        model_folder=args.model_folder,)
+
+    eval_num = args.num
+    score = evaluation(env, agent, args.render_last, eval_num)
+
+    print("Avg_score:  ", np.mean(score))
+    winrate: float = np.count_nonzero(score > 0) / eval_num
+    print("Avg win rate:  ", winrate)
+    # print("Avg_highest:", np.sum(highest) / eval_num)
+    # calculate (1-alpha)% confidence interval with {win_count} successes in
+    # {num_simulations} trials
+    print(f'The {1-args.significance_level} confidence interval: {proportion_confint(count=winrate, nobs=eval_num, alpha=args.significance_level)}')
+
+    print(f"Counts: (Total of {eval_num} rollouts)")
